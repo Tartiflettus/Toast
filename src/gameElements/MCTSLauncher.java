e1d728fae828c351922ebd266b2925d915f98601
Creusel <victor.creusel@gmail.com>
2017-10-12 17:45:53 +0200

    Re-dépot de MCTSLauncher et rapport suite à une erreur

---
+++ src/gameElements/MCTSLauncher.java
@@ -0,0 +1,39 @@
+package gameElements;
+
+public class MCTSLauncher implements Runnable {
+	
+	private Game game;
+	
+	private static final long DELAI = 500;
+	
+	public MCTSLauncher(Game game) {
+		this.game = game;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 * Lance le calcul de MCTS
+	 */
+	@Override
+	public void run() {
+		game.attendre();
+		
+		ArbreMonteCarlo arbre = game.getProchainCoup();
+		arbre.developper();
+		final long start = System.currentTimeMillis();
+		final long end = start + DELAI;
+		int cptMCTS = 0;
+		while(System.currentTimeMillis() < end){
+			arbre.MCTS();
+			++cptMCTS;
+		}
+		game.setCptMCTS(cptMCTS);
+		game.setProchainCoup(arbre);
+		
+		game.finCalculOrdi();
+		
+		game.ordiQuiJoue();
+	}
+
+}
